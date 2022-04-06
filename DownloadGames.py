02d1ada95fc364fea647bd3c49cbf14e7a9e09b7
Tim Moore <timoore92@gmail.com>
2022-04-06 16:56:28 -0300

    Added the function to download games

---
+++ DownloadGames.py
@@ -0,0 +1,37 @@
+"""NHL Game Data Downloader
+
+The function in this file will download the data produced by the nhl for all game sinthe requested season and
+dump a seperate CSV for each season in a folder called "hockey_scraper_data" from where the file was run.
+
+Author: Tim Moore
+"""
+
+import hockey_scraper
+import os
+
+def downloadGames(years):
+    """
+    Scrape the data for the years required for calculation
+
+    :param years: Years of games you wish to download
+
+    :return: None
+    """
+
+    path = os.getcwd()
+    USER_PATH = os.join(path,"hockey_scraper_data")
+
+    if os.path.exists(USER_PATH):
+        print("Path is good")
+    else:
+        os.mkdir(USER_PATH)
+        print("Path created")
+
+    for year in years:
+        hockey_scraper.scrape_seasons([year], False, docs_dir=USER_PATH)
+
+if __name__ == '__main__':
+    years_to_download = [2020,2021]
+    downloadGames(years_to_download)
+
+
