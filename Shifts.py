ca2d367f1039d75305f25cc69d4b14d0471d4cdf
Tim Moore <timoore92@gmail.com>
2022-04-18 16:59:27 -0300

    Shift.py progress
    Further clean up raw data merge all data from shift into 1 shift showing, players, goals, and time on ice
    
    *Minor updates*

--- Shifts.py
+++ Shifts.py
@@ -8,9 +8,36 @@ Date: 22-04-12
 import pandas as pd
 
 def shifts(clean_data):
+    check_cols = ['period',
+                  'awayPlayer1_id',
+                  'awayPlayer2_id',
+                  'awayPlayer3_id',
+                  'awayPlayer4_id',
+                  'awayPlayer5_id',
+                  'awayPlayer6_id',
+                  'homePlayer1_id',
+                  'homePlayer2_id',
+                  'homePlayer3_id',
+                  'homePlayer4_id',
+                  'homePlayer5_id',
+                  'homePlayer6_id',]
+    home_cols = ['homePlayer1_id',
+                 'homePlayer2_id',
+                 'homePlayer3_id',
+                 'homePlayer4_id',
+                 'homePlayer5_id',
+                 'homePlayer6_id',]
     shift_df = pd.Dataframe()
     for i, row in clean_data.iterrows():
         if i == 0:
             shift_start = row
-        if shift_start["period"] != row["period"]:
+
+        goal = 0
+        if row['Event'] == "Goal":
+            if row["p1_id"] in row[home_cols]:
+                goal += 1
+            else:
+                goal -= 1
+
+        if shift_start[check_cols] != row[check_cols]:
 
