e2c36adf761574f7eb3f32dda8045124c5769beb
Tim Moore <timoore92@gmail.com>
2022-04-22 17:03:37 -0300

    Shift.py progress
    Further clean up raw data merge all data from shift into 1 shift showing, players, goals, and time on ice
    
    Shift.py Complete

---
+++ ShiftMatrix.py
@@ -0,0 +1,9 @@
+"""Create: Time on ice matrix (diagonal shift size matrix with shifts times)
+            Players shift matrix (player x shifts matrix with 1 if the players plays home team on the shift -1 if away)
+            Goal Matrix (A shift x 1 matrix with a 1 if home team scores, -1 if way team scores
+
+The function in this file will load in the Shift CSV and out put 3 matrices for final calculations
+
+Author: Tim Moore
+Date: 22-04-22
+"""
