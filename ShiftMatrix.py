195101efaceaa809c6efeb8c217d8440151f1f09
Tim Moore <timoore92@gmail.com>
2022-04-27 16:45:41 -0300

    ShiftMatrix.py progress
    Updated Shifts.py and began building ShiftMatrix.py

--- ShiftMatrix.py
+++ ShiftMatrix.py
@@ -13,13 +13,38 @@ def shifts_converter(shift):
     shift_dict = {}
     shift = shift.drop(['Unnamed: 0','Time', 'Goals'], axis = 1)
     np_shift = shift.to_numpy().astype(int)
-    print(np_shift)
+
     shift_index = 0
     for i in np_shift:
         shift_player = 0
         for j in i:
+            if j not in shift_dict:
+                shift_dict[j] = [[],[]]
             if 0 <= shift_player <= 5:
-                if j not in shift_dict
+                shift_dict[j][0].append(shift_index)
+            else:
+                shift_dict[j][1].append(shift_index)
+            shift_player += 1
+        shift_index += 1
+
+
+    shift_list = []
+    for key, val in shift_dict.items():
+        shift_list.append([key, val])
+
+    shift_list.sort()
+
+    shift_matrix = np.zeros((len(shift_list),len(shift)),dtype=int)
+    for i in range(len(shift_list)):
+        for j in shift_list[i][1][0]:
+            shift_matrix[i][j] = -1
+        for j in shift_list[i][1][1]:
+            shift_matrix[i][j] = 1
+
+
+    print(np.shape(shift_matrix))
+
+    return shift_matrix, np.absolute(shift_matrix)
 
 
 if __name__ == '__main__':
@@ -27,5 +52,6 @@ if __name__ == '__main__':
     directory = os.path.join(path,"hockey_scraper_data","csvs")
     with open(os.path.join(directory,"Shifts.csv"), "+r") as f:
         current_df = pd.read_csv(f)
-    shift_mat  = shifts_converter(current_df)
-    #shift_mat.to_csv(os.path.join(directory,'ShiftMatrix.csv'))
+    net_shift_mat, tot_shift_mat  = shifts_converter(current_df)
+    #net_shift_mat.tofile(os.path.join(directory,'NetShiftMatrix.csv'), sep = ',')
+    #tot_shift_mat.tofile(os.path.join(directory,'TotalShiftMatrix.csv'), sep = ',')
